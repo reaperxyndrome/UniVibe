6d448d795e2c936336f7644e1c0c04180de646d6
Leon <doncorleone142857@gmail.com>
2023-12-05 09:25:05 +0700

    added contact form

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -1,6 +1,30 @@
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
+function ContactForm() {
+	return(
+		<div className="mt-8">
+			<h3 className="text-xl font-semibold mb-4">Send us a message</h3>
+			<form>
+				<div className="mb-4">
+					<label htmlFor="name" className="block text-gray-600 mb-2">Name</label>
+					<input type="text" id="name" className="w-full px-3 py-2 border rounded shadow appearance-none text-gray-600 leading-tight focus:outline-none focus:shadow-outline" />
+				</div>
+				<div className="mb-4">
+					<label htmlFor="email" className="block text-gray-600 mb-2">Email</label>
+					<input type="email" id="email" className="w-full px-3 py-2 border rounded shadow appearance-none text-gray-600 leading-tight focus:outline-none focus:shadow-outline" />
+				</div>
+				<div className="mb-4">
+					<label htmlFor="message" className="block text-gray-600 mb-2">Message</label>
+					<textarea id="message" rows="4" className="w-full px-3 py-2 border rounded shadow appearance-none text-gray-600 leading-tight focus:outline-none focus:shadow-outline"></textarea>
+				</div>
+				<button type="submit" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+					Send
+				</button>
+			</form>
+		</div>
+	)
+}
 function Contact() {
 	return (
 		<div className="flex flex-col min-h-screen bg-black text-white">
@@ -27,6 +51,7 @@ function Contact() {
 			</p>
 			</div>
 		</main>
+		<ContactForm></ContactForm>
 		<Footer></Footer>
 		</div>
 	);
