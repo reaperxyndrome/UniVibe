fad338af4603cdfa79b94bb12283f50587123509
Leon <doncorleone142857@gmail.com>
2023-12-05 10:36:51 +0700

    refactoring

--- src/SignIn.jsx
+++ src/SignIn.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "./StaticNavbar";
 import Footer from "./Footer";
 function SignIn() {
     const [formData, setFormData] = useState({
--- src/SignUp.jsx
+++ src/SignUp.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Navbar from "./Navbar";
+import Navbar from "./StaticNavbar";
 import Footer from "./Footer";
 function SignUp() {
     const [formData, setFormData] = useState({
