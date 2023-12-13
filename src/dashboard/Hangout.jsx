9d1e51be8b86bcae3ce041f1e928cc66565042f9
filbertfederico <filbertblack@gmail.com>
2023-12-13 20:25:59 +0700

    initial code

---
+++ src/dashboard/Hangout.jsx
@@ -0,0 +1,205 @@
+// import * as React from "react";
+
+export default function Hangout() {
+    return (
+
+    <div className="bg-white flex justify-between gap-5 pl-20 pr-6 pt-12 items-start max-md:flex-wrap max-md:px-5">
+      <div className="flex grow basis-[0%] flex-col items-center mt-4 max-md:max-w-full">
+        <div className="flex-col fill-[linear-gradient(253deg,#2563EB_36.4%,rgba(37,99,235,0.00)_68.71%),#1F2937] overflow-hidden relative flex min-h-[99px] w-[522px] justify-center items-center px-px max-md:max-w-full">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/5092347e53bfa621db95507f6b53ac938290f5ed2d14bf002acb4e95fd644f03?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+            className="absolute h-full w-full object-cover object-center inset-0"
+          />
+          <div className="flex-col relative fill-[linear-gradient(253deg,#2563EB_36.4%,rgba(37,99,235,0.00)_68.71%),#1F2937] overflow-hidden flex min-h-[99px] w-full items-stretch pl-2.5 pr-6 py-2 max-md:max-w-full max-md:pr-5">
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/79ebc522b4c4774c60ae05d8d4b4571376f8244c602980c473a2400aedc8e214?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+              className="absolute h-full w-full object-cover object-center inset-0"
+            />
+            <div className="relative flex justify-between gap-2.5 px-0.5 items-start max-md:max-w-full max-md:flex-wrap max-md:justify-center">
+              <div className="flex grow basis-[0%] flex-col items-stretch">
+                <div className="text-white text-base font-bold whitespace-nowrap">
+                  Nobar
+                </div>
+                <div className="text-neutral-300 text-xs font-medium mt-3">
+                  Due Date
+                </div>
+                <div className="text-white text-xs font-semibold whitespace-nowrap mt-2.5">
+                  10 Oktober, 2023
+                </div>
+              </div>
+              <div className="flex basis-[0%] flex-col items-stretch mt-7 self-end">
+                <div className="text-neutral-300 text-xs font-medium whitespace-nowrap">
+                  Time
+                </div>
+                <div className="text-white text-xs font-semibold whitespace-nowrap mt-2.5">
+                  13.45
+                </div>
+              </div>
+              <div className="flex grow basis-[0%] flex-col items-stretch mt-6 self-end">
+                <div className="text-neutral-300 text-xs font-medium whitespace-nowrap">
+                  Location
+                </div>
+                <div className="text-white text-xs font-semibold mt-2.5">
+                  Mall Grand Indonesia
+                </div>
+              </div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/21e71f6561d2501b4d82959d5438683b7834636ce4778df43e0710d3c11fc39e?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+                className="aspect-[3.07] object-contain object-center w-[89px] overflow-hidden shrink-0 max-w-full"
+              />
+            </div>
+            <div className="relative text-white text-xs font-medium bg-orange-500 justify-center mt-1.5 pl-5 pr-16 py-2 rounded-2xl items-start max-md:max-w-full max-md:px-5">
+              film the creator jam 13.45
+            </div>
+          </div>
+        </div>
+        <div className="flex-col fill-[linear-gradient(253deg,#2563EB_36.4%,rgba(37,99,235,0.00)_68.71%),#1F2937] overflow-hidden relative flex min-h-[99px] w-[522px] items-stretch mt-3 pl-3.5 pr-6 py-2 max-md:max-w-full max-md:pr-5">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/2726924dfbc01c2538b7ea10452e0cb035ade0e35b15d0ad1198cf3c88344c2e?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+            className="absolute h-full w-full object-cover object-center inset-0"
+          />
+          <div className="relative flex justify-between gap-5 px-0.5 items-start max-md:max-w-full max-md:flex-wrap max-md:justify-center">
+            <div className="flex grow basis-[0%] flex-col items-stretch">
+              <div className="text-white text-base font-bold">Basket kuyy</div>
+              <div className="text-neutral-300 text-xs font-medium whitespace-nowrap mt-2">
+                Due Date
+              </div>
+              <div className="text-white text-xs font-semibold mt-2.5">
+                13 Oktober, 2023
+              </div>
+            </div>
+            <div className="flex basis-[0%] flex-col items-stretch mt-7 self-end">
+              <div className="text-neutral-300 text-xs font-medium whitespace-nowrap">
+                Time
+              </div>
+              <div className="text-white text-xs font-semibold whitespace-nowrap mt-2.5">
+                12.10
+              </div>
+            </div>
+            <div className="flex grow basis-[0%] flex-col items-stretch mt-6 self-end">
+              <div className="text-neutral-300 text-xs font-medium">
+                Location
+              </div>
+              <div className="text-white text-xs font-semibold mt-2.5">
+                Fx Sudirman
+              </div>
+            </div>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/885461b625bfd55a917007c4e9ef65393660c72876d4ab17df5e4f2897c584ae?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+              className="aspect-[3.07] object-contain object-center w-[89px] overflow-hidden shrink-0 max-w-full"
+            />
+          </div>
+          <div className="relative text-white text-xs font-medium bg-orange-500 justify-center mt-1.5 pl-5 pr-16 py-2.5 rounded-2xl items-start max-md:max-w-full max-md:pr-5">
+            CGV court
+          </div>
+        </div>
+        <div className="flex-col fill-[linear-gradient(253deg,#2563EB_36.4%,rgba(37,99,235,0.00)_68.71%),#1F2937] overflow-hidden relative flex min-h-[99px] w-[522px] items-stretch mt-3.5 pl-2.5 pr-6 py-2 max-md:max-w-full max-md:pr-5">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/46618432172df705418d0964be2a57c51e8aa671df5b74ade57c2293fff3f2a3?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+            className="absolute h-full w-full object-cover object-center inset-0"
+          />
+          <div className="relative flex items-start justify-between gap-5 pr-1.5 max-md:max-w-full max-md:flex-wrap max-md:justify-center">
+            <div className="flex grow basis-[0%] flex-col items-stretch self-start">
+              <div className="text-white text-base font-bold">Mabar</div>
+              <div className="text-neutral-300 text-xs font-medium whitespace-nowrap mt-3">
+                Due Date
+              </div>
+              <div className="text-white text-xs font-semibold mt-2.5">
+                21 Oktober, 2023
+              </div>
+            </div>
+            <div className="flex basis-[0%] flex-col items-stretch mt-7 self-end">
+              <div className="text-neutral-300 text-xs font-medium whitespace-nowrap">
+                Time
+              </div>
+              <div className="text-white text-xs font-semibold whitespace-nowrap mt-2.5">
+                19.00
+              </div>
+            </div>
+            <div className="flex grow basis-[0%] flex-col items-stretch mt-6 self-end">
+              <div className="text-neutral-300 text-xs font-medium">
+                Location
+              </div>
+              <div className="text-white text-xs font-semibold mt-2.5">
+                Online
+              </div>
+            </div>
+            <img
+              loading="lazy"
+              src="https://cdn.builder.io/api/v1/image/assets/TEMP/301d6c91b39fad986d822a95a1e27987d020af401f20beac9ee11f2f88b4f7b7?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+              className="aspect-[3.07] object-contain object-center w-[89px] overflow-hidden self-center shrink-0 max-w-full my-auto"
+            />
+          </div>
+          <div className="relative text-white text-xs font-medium bg-orange-500 justify-center mt-1.5 pl-5 pr-16 py-2.5 rounded-2xl items-start max-md:max-w-full max-md:pr-5">
+            MPLS sabtu
+          </div>
+        </div>
+        <div className="flex-col fill-[linear-gradient(253deg,#2563EB_36.4%,rgba(37,99,235,0.00)_68.71%),#1F2937] overflow-hidden relative flex min-h-[79px] w-[522px] items-stretch mt-4 pl-2.5 pr-6 pt-2.5 max-md:max-w-full max-md:pr-5">
+          <img
+            loading="lazy"
+            src="https://cdn.builder.io/api/v1/image/assets/TEMP/1b4b91aa4f593938768897ea28287b960c11074b3fe90b690b80aa7088909849?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+            className="absolute h-full w-full object-cover object-center inset-0"
+          />
+          <div className="relative flex w-full items-stretch justify-between gap-5 max-md:max-w-full max-md:flex-wrap">
+            <div className="flex grow basis-[0%] flex-col items-stretch">
+              <div className="flex justify-between gap-5 items-start">
+                <div className="flex flex-col items-stretch">
+                  <div className="text-white text-base font-bold">Nobar</div>
+                  <div className="text-neutral-300 text-xs font-medium whitespace-nowrap mt-3">
+                    Due Date
+                  </div>
+                </div>
+                <div className="text-neutral-300 text-xs font-medium mt-7 self-end">
+                  Time
+                </div>
+              </div>
+              <div className="flex justify-between gap-5 mt-1.5 items-start">
+                <div className="text-white text-xs font-semibold">
+                  10 Oktober, 2023
+                </div>
+                <div className="text-white text-xs font-semibold mt-1">
+                  13.45
+                </div>
+              </div>
+            </div>
+            <div className="self-center flex gap-2.5 my-auto px-0.5 items-start">
+              <div className="flex grow basis-[0%] flex-col items-stretch mt-4">
+                <div className="text-neutral-300 text-xs font-medium whitespace-nowrap">
+                  Location
+                </div>
+                <div className="text-white text-xs font-semibold mt-2.5">
+                  Mall Grand Indonesia
+                </div>
+              </div>
+              <img
+                loading="lazy"
+                src="https://cdn.builder.io/api/v1/image/assets/TEMP/c26c0ed6a7b21b02cb64aa462947480369c5426f1bda8e6cc5a7041a214df2e4?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+                className="aspect-[3.07] object-contain object-center w-[89px] overflow-hidden shrink-0 max-w-full"
+              />
+            </div>
+          </div>
+          <div className="relative text-white text-xs font-medium bg-orange-500 mt-1.5 pl-5 pr-16 pt-2 rounded-2xl items-start max-md:max-w-full max-md:px-5">
+            film the creator jam 13.45
+          </div>
+        </div>
+      </div>
+      <div className="flex basis-[0%] flex-col items-stretch mt-80 self-end max-md:mt-10">
+        <div className="text-black text-base font-bold whitespace-nowrap">
+          HOST
+        </div>
+        <img
+          loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&width=2000 2000w, https://cdn.builder.io/api/v1/image/assets/TEMP/9f45716ece8789c3587414538992581a2479d125566a777f4dd421855524aa2e?apiKey=b1110ce74f26469f8c7944650aef6c79&"
+          className="aspect-square object-contain object-center w-[53px] overflow-hidden self-center mt-2"
+        />
+      </div>
+    </div>
+  );
+}
+
