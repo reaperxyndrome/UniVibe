5e0070921122efdbd3fe78f38aeb77921dca0ff5
DreemGoat <daniel.phangandy@binus.ac.id>
2024-01-06 19:23:57 +0700

    Added Dashboard Components

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -1,3 +1,4 @@
+import DashboardComponents from "./DashboardComponents"
 import DashboardNavbar from "./DashboardNavbar";
 import DashboardSidebar from "./DashboardSidebar";
 import Footer from "../Footer";
@@ -6,6 +7,7 @@ function Dashboard() {
     return (
       <div className="flex flex-col h-screen overflow-hidden bg-gray-100">
           <DashboardNavbar></DashboardNavbar>
+          <DashboardComponents></DashboardComponents>
           <DashboardSidebar></DashboardSidebar>
           <Footer></Footer>
       </div>
