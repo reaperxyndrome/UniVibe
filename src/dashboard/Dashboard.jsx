acc8d09071b7249ab7253349ece9deef780a2b2f
Leon <doncorleone142857@gmail.com>
2024-01-09 01:11:57 +0700

    indentation

--- src/dashboard/Dashboard.jsx
+++ src/dashboard/Dashboard.jsx
@@ -6,12 +6,12 @@ import Footer from "../Footer";
 function Dashboard() {
     return (
       <div className="flex flex-col h-screen overflow-hidden bg-gray-100">
-          <DashboardNavbar></DashboardNavbar>
-          <div className="flex">
+        <DashboardNavbar></DashboardNavbar>
+        <div className="flex">
           <DashboardSidebar></DashboardSidebar>
           <DashboardComponents></DashboardComponents>
-          </div>
-          <Footer></Footer>
+        </div>
+        <Footer></Footer>
       </div>
     );
   }
