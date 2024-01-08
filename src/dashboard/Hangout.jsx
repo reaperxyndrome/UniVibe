5b4a193814927fac3787ab982c243f95d4a617ab
Leon <doncorleone142857@gmail.com>
2024-01-09 04:54:25 +0700

    hangout refactoring

--- src/dashboard/Hangout.jsx
+++ src/dashboard/Hangout.jsx
@@ -1,25 +1,20 @@
 // import * as React from "react";
 import { Link } from 'react-router-dom';
 
-export default function Hangout() {
+export default function HangoutPage() {
     return (
-        <div className="items-center bg-white flex max-w-[1217px] flex-col justify-center mx-auto w-50">
+        <div className="flex items-center bg-white max-w-[1217px] flex-col justify-center mx-auto w-50">
           <div className="flex w-[522px] max-w-full flex-col items-center">
  
             {/* box 1 */}
-            <div className="flex-col fill-gray-800 overflow-hidden relative flex min-h-[99px] w-full items-stretch mt-3 pl-3.5 pr-6 py-2 max-md:max-w-full max-md:pr-5">
-              <img
-                loading="lazy"
-                src="https://cdn.builder.io/api/v1/image/assets/TEMP/6b7d28693f13d9f21c26489d01ef02051bc44cc21475fbe70f7f1a8f83e36d23?apiKey=b1110ce74f26469f8c7944650aef6c79&"
-                className="absolute h-full w-full object-cover object-center inset-0"
-              />
+            <div className="flex flex-col bg-gray-800 w-full rounded-xl pl-3.5 pr-6 py-4 max-md:max-w-full max-md:pr-5">
               <div className="relative flex justify-between gap-5 px-0.5 items-start max-md:max-w-full max-md:flex-wrap max-md:justify-center">
                 <div className="flex grow basis-[0%] flex-col items-stretch">
                   <div className="text-white text-base font-bold">Nobar</div>
                   <div className="text-neutral-300 text-xs font-medium whitespace-nowrap mt-2">
                     Due Date
                   </div>
-                  <div className="text-white text-xs font-semibold mt-2.5">
+                  <div className="text-white text-xs font-semibold">
                     10 Oktober, 2023
                   </div>
                 </div>
@@ -27,7 +22,7 @@ export default function Hangout() {
                   <div className="text-neutral-300 text-xs font-medium whitespace-nowrap">
                     Time
                   </div>
-                  <div className="text-white text-xs font-semibold whitespace-nowrap mt-2.5">
+                  <div className="text-white text-xs font-semibold whitespace-nowrap">
                     13.45
                   </div>
                 </div>
@@ -35,15 +30,12 @@ export default function Hangout() {
                   <div className="text-neutral-300 text-xs font-medium">
                     Location
                   </div>
-                  <div className="text-white text-xs font-semibold mt-2.5">
+                  <div className="text-white text-xs font-semibold">
                     Mall Grand Indonesia
                   </div>
                 </div>
-                <img
-                  loading="lazy"
-                  src="https://cdn.builder.io/api/v1/image/assets/TEMP/885461b625bfd55a917007c4e9ef65393660c72876d4ab17df5e4f2897c584ae?apiKey=b1110ce74f26469f8c7944650aef6c79&"
-                  className="aspect-[3.07] object-contain object-center w-[89px] overflow-hidden self-center shrink-0 max-w-full"
-                />
+                <button className='bg-[#2e3f40] text-[#00fca9] px-5 py-1 rounded-md text-sm'>Join</button>
+                <></>
               </div>
               <div className="relative text-white text-xs font-medium bg-orange-500 justify-center mt-1.5 pl-5 pr-16 py-2.5 rounded-2xl items-start max-md:max-w-full max-md:pr-5">
                 film the creator jam 13.45
