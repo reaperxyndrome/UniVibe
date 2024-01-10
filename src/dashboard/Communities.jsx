6100c2d33cc903f8b3d6a0beb7312a2f570e88ff
brilianvy2 <brilianvincenza8@gmail.com>
2024-01-10 23:44:53 +0700

    fixing syntax error

--- src/dashboard/Communities.jsx
+++ src/dashboard/Communities.jsx
@@ -7,22 +7,22 @@ import CommunityCard from "./CommunityCard";
 function Communities() {
   return (
     <div className="flex flex-col h-screen overflow-hidden bg-gray-100">
-      // This is the Dashboard's Navbar which can be found at the top of the page
+      {/* This is the Dashboard's Navbar which can be found at the top of the page */}
       <DashboardNavbar></DashboardNavbar>
       <div className="flex">
-        // This is the Dashboard's Sidebar which can be found on the side of the page
+        {/* This is the Dashboard's Sidebar which can be found on the side of the page */}
         <DashboardSidebar></DashboardSidebar>
         <div className="flex flex-col items-center p-7">
-          // This is the title of the page which is "Communities"
+          {/* This is the title of the page which is "Communities" */}
           <h1 className="text-3xl font-bold mb-5">Communities</h1>
-          // This is the button to create new community
+          {/* This is the button to create new community */}
           {/* Create Community Button */}
           <button onClick={handleCreate} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Create Community
           </button>
           
           <div className="flex flex-wrap gap-5 mt-5">
-            // This is the card to show the community
+            {/* This is the card to show the community */}
             <CommunityCard />
             <CommunityCard />
             <CommunityCard />
--- src/dashboard/CommunityCard.jsx
+++ src/dashboard/CommunityCard.jsx
@@ -3,16 +3,16 @@ function CommunityCard(){
     return(
         // Contains all the elements of a community card in Communities.jsx
         <div className="flex flex-col items-center rounded-lg shadow-md bg-[antiquewhite] w-56 p-5">
-            // An image element which displays the community's logo or picture
+            {/* An image element which displays the community's logo or picture */}
             <img src="" className="w-[60px] h-[60px] bg-black relative"></img>
             <div className="text-center mt-4 mb-5">
-                // A heading for the community's name
+                {/* A heading for the community's name */}
                 <h2 className="font-bold mb-3">Community Name</h2>
-                // A paragraph which contains the community's description
+                {/* A paragraph which contains the community's description */}
                 <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum augue sit amet neque varius.
                 </p>
             </div>
-            // A button that users can click to join a community
+            {/* A button that users can click to join a community */}
             <button className=" p-2 px-4 rounded-md bg-white">Join this community</button>
         </div>
     )
