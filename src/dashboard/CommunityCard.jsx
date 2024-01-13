469545ee29f931a380e0c891bd39738e89f3be7c
kevin matthew T <kmturbobro02@gmail.com>
2024-01-13 21:32:10 +0700

    community card ui changes

--- src/dashboard/CommunityCard.jsx
+++ src/dashboard/CommunityCard.jsx
@@ -8,7 +8,7 @@ function CommunityCard(props){
             <img src={props.icon} className="w-[60px] h-[60px] bg-black relative"></img>
             <div className="text-center mt-4 mb-5">
                 <h2 className="font-bold mb-3">{props.name}</h2>
-                <p className="text-sm">{props.description}
+                <p className="text-sm text-black">{props.description}
                 </p>
             </div>
             <button className=" p-2 px-4 rounded-md bg-white" onClick={()=>{navigate('/dashboard/'+props.chatname)}}>Join this community</button>
